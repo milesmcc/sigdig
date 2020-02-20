6b6e7dac6ff67edfdf3867d6573cb930595fff3a
R. Miles McCain <x1-key@sendmiles.email>
2020-02-20 13:48:30 -0500

    Add flush option

--- src/codecs/mod.rs
+++ src/codecs/mod.rs
@@ -25,19 +25,25 @@ pub trait Codec {
     {
         let mut total_bytes_written: u64 = 0;
         let mut buffer = [0 as u8; DEFAULT_BUFFER_SIZE];
-        loop {
+        'read: loop {
             let len = match reader.read(&mut buffer) {
-                Ok(0) => return Ok(total_bytes_written),
+                Ok(0) => break 'read,
                 Ok(len) => len,
                 Err(e) => return Err(CodecError::ReadError(e)),
             };
-            writer.write_all(self.transform(Vec::from(&mut buffer[..len]))?.as_mut_slice())?;
+            writer.write_all(self.process(Vec::from(&mut buffer[..len]))?.as_mut_slice())?;
             total_bytes_written += len as u64;
         }
+        writer.write_all(self.flush()?.as_mut_slice())?;
+        Ok(total_bytes_written)
     }
 
-    fn transform(&mut self, buffer: Vec<u8>) -> Result<Vec<u8>, CodecError> {
-        return Ok(buffer)
+    fn process(&mut self, buffer: Vec<u8>) -> Result<Vec<u8>, CodecError> {
+        return Ok(buffer);
+    }
+
+    fn flush(&mut self) -> Result<Vec<u8>, CodecError> {
+        return Ok(Vec::new());
     }
 }
 
--- src/codecs/not.rs
+++ src/codecs/not.rs
@@ -9,7 +9,7 @@ impl Not {
 }
 
 impl Codec for Not {
-    fn transform(&mut self, buffer: Vec<u8>) -> Result<Vec<u8>, CodecError> {
+    fn process(&mut self, buffer: Vec<u8>) -> Result<Vec<u8>, CodecError> {
         let mut bytes = Vec::with_capacity(buffer.len());
         for byte in buffer {
             bytes.push(!byte);
